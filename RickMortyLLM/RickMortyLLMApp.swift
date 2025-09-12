bdd32bedc9c9b5af488e88bed002007f6f3b9649
Joshua Cleetus <joshua.cleetus@gmail.com>
2025-09-12 17:39:53 -0400

    Initial Commit

---
+++ RickMortyLLM/RickMortyLLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyLLMApp.swift
+//  RickMortyLLM
+//
+//  Created by Joshua Cleetus on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyLLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
